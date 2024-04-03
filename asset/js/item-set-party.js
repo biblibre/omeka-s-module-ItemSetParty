4334225ff2eb15903fb0a3f67cb5ece629232c48
Thibaud Guillot <thibaud.guillot@biblibre.com>
2024-04-03 14:21:07 +0200

    Fix bug #3 : Review AJAX logic calls to reduce number of request

--- asset/js/item-set-party.js
+++ asset/js/item-set-party.js
@@ -7,65 +7,57 @@
         let defaultSidebarHtml = null;
         let sidebarActive = false;
 
-        function initEvent() {
-            $('.archival_resource').on('click', function (e) {
-                e.preventDefault();
-                const element = $(this);
-                const parentElement = element.parent();
-                if (!parentElement.closest('div').hasClass('archival_tree_head')) {
-                    const id = parentElement.data('resource-id');
-                    const resource_type = parentElement.data('resource-type');
-
-                    $.ajax({
-                        url: `item-set-party/relations/${resource_type}/${id}`,
-                    }).done(function (data) {
-                        const $data = $(data);
-
-                        $data.find('li').each(function () {
-                            const liId = $(this).find('a').attr('id');
-                            if (!parentElement.find(`#${liId}`).length) {
-                                const list = $("<ul></ul>");
-                                list.append(this);
-                                parentElement.append(list);
-                            }
-                        });
-                        initEvent();
-                    });
-                }
-                element.addClass('parent_active');
-            });
-
-            $('.resource_details_view').on('click', function (e) {
-                e.preventDefault();
-                const sidebar = $("#resource-details");
-                const sidebarContent = sidebar.find('#sidebar-content');
-
-                if (!sidebar.hasClass('active')) {
-                    defaultSidebarHtml = sidebarContent.html();
-                }
-                const parentElement = $(this).parent();
+        $(document).on('click', '.archival_resource', function (e) {
+            e.preventDefault();
+            const element = $(this);
+            const parentElement = element.parent();
+            if (!parentElement.closest('div').hasClass('archival_tree_head')) {
                 const id = parentElement.data('resource-id');
                 const resource_type = parentElement.data('resource-type');
-
                 $.ajax({
-                    url: `item-set-party/resource-details/${resource_type}/${id}`,
+                    url: `item-set-party/relations/${resource_type}/${id}`,
                 }).done(function (data) {
-                    if (sidebarActive) {
-                        sidebarContent.html(data);
-                    } else {
-                        sidebar.addClass('active');
-                        sidebarContent.html(data);
-                        sidebarActive = true;
-                    }
+                    const $data = $(data);
+                    $data.find('li').each(function () {
+                        const liId = $(this).find('a').attr('id');
+                        if (!parentElement.find(`#${liId}`).length) {
+                            const list = $("<ul></ul>");
+                            list.append(this);
+                            parentElement.append(list);
+                        }
+                    });
                 });
+            }
+            element.addClass('parent_active');
+        });
 
-                $('.sidebar-close').on('click', function () {
-                    const sidebar = $("#resource-details");
-                    sidebar.removeClass('active');
-                    sidebarActive = false;
-                });
+        $(document).on('click', '.resource_details_view', function (e) {
+            e.preventDefault();
+            const sidebar = $("#resource-details");
+            const sidebarContent = sidebar.find('#sidebar-content');
+            if (!sidebar.hasClass('active')) {
+                defaultSidebarHtml = sidebarContent.html();
+            }
+            const parentElement = $(this).parent();
+            const id = parentElement.data('resource-id');
+            const resource_type = parentElement.data('resource-type');
+            $.ajax({
+                url: `item-set-party/resource-details/${resource_type}/${id}`,
+            }).done(function (data) {
+                if (sidebarActive) {
+                    sidebarContent.html(data);
+                } else {
+                    sidebar.addClass('active');
+                    sidebarContent.html(data);
+                    sidebarActive = true;
+                }
             });
-        }
-        initEvent();
-    });
+        });
+
+        $(document).on('click', '.sidebar-close', function () {
+            const sidebar = $("#resource-details");
+            sidebar.removeClass('active');
+            sidebarActive = false;
+        });
+    });   
 })(jQuery);
